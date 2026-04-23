83ad120e68632ad83867bbfd2df5979c67fb10e0
generatedscript <giandumpit17@gmail.com>
2026-04-23 13:39:31 +0800

    e

--- framework/src/Shared/Database/WeaponStats.lua
+++ framework/src/Shared/Database/WeaponStats.lua
@@ -1,20 +1,6 @@
 --!strict
 
-local WeaponStats = {}
+local root = script.Parent.Parent.Parent
+local SharedConfig = require(root.Shared.common.Config)
 
-WeaponStats.Shotgun = {
-	Pellets = 8,
-	Range = 400,
-	PelletDamage = 12,
-	SpreadDegrees = 6,
-	MuzzleVelocity = 800,
-	FireCooldown = 0.9,
-}
-
-WeaponStats.Knife = {
-	Range = 7,
-	Damage = 55,
-	SwingCooldown = 0.45,
-}
-
-return WeaponStats
+return SharedConfig.WeaponStats
